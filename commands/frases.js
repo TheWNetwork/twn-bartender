69d133e45255916f64fd51018ff180b47ea215fd
Kevin Cala Sánchez <kevin@kcsystem.es>
2022-07-06 20:06:34 +0200

    language when no language

--- commands/frases.js
+++ commands/frases.js
@@ -1,5 +1,6 @@
 module.exports.run = async (botconfig, pool, bot, message, args) => {
-    let qry = `SELECT frase, rand() as random FROM frases WHERE deleted = 0 AND lang = '${message.from.language_code}' ORDER BY 2 LIMIT 1;`;
+    let lang = typeof message.from.language_code === 'undefined' ? 'es' : message.from.language_code;
+    let qry = `SELECT frase, rand() as random FROM frases WHERE deleted = 0 AND lang = '${lang}' ORDER BY 2 LIMIT 1;`;
     let dbRes = await pool.query(qry);
     return dbRes[0].frase;
 };
